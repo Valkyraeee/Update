0f44fcbd9a814d226418205dd8d3103d03d05def
Valkyraeee <114802352+Valkyraeee@users.noreply.github.com>
2025-09-26 13:45:59 +0800

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyFirstApplication.Models;
+
+namespace MyFirstApplication.Data
+{
+    public class ApplicationDbContext: DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<GatePass> GatePasses { get; set; }
+        public DbSet<LockerRequest> LockerRequests { get; set; }
+        public DbSet<ActivityReservation> ActivityReservations { get; set; }
+
+
+    }
+}
